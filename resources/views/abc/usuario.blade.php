13ffaa06a796bd0301bd5feb5cb1dbad9182db90
Eiji De Paz <pruebas@prueba.gt>
2015-10-16 02:35:44 -0600

    pagina principal de abc y sincronizacion de abcs

--- resources/views/abc/usuario.blade.php
+++ resources/views/abc/usuario.blade.php
@@ -36,7 +36,7 @@
 
 	{!! Form::open(['url' => 'b_usuario']) !!}
 		{!! Form::label('Usuario:') !!}
-		{!! Form::select('usuario',$opciones2, array_values($opciones2)[0]) !!}
+		{!! Form::select('usuario',$opciones, array_values($opciones)[0]) !!}
 		{!! Form::submit('Borrar') !!}
 	{!! Form::close() !!}
 
@@ -44,7 +44,7 @@
 	<hr>
 	{!! Form::open(['url' => 'c_usuario']) !!}
 		{!! Form::label('Usuario a actualizar:') !!}
-		{!! Form::select('usuario',$opciones2, array_values($opciones2)[0]) !!}
+		{!! Form::select('usuario',$opciones, array_values($opciones)[0]) !!}
 		{!! Form::label('Password:') !!}
 		{!! Form::text('password') !!}
 		{!! Form::label('Nombre:') !!}
@@ -64,5 +64,12 @@
 
 @section('nav_items')
 	<li><a href="/">Principal</a></li>
-	<li class="current"><a href="#">Registro</a></li>
+	<li><a href="/abc_establecimiento">Establecimiento</a></li>
+	<li><a href="/abc_tipo_establecimiento">Tipo Establecimiento</a></li>
+	<li><a href="/abc_servicio">Servicio</a></li>
+	<li><a href="/abc_tipo_servicio">Tipo Servicio</a></li>
+	<li><a href="/abc_dimension">Dimension</a></li>
+	<li><a href="/abc_categoria">Categoria</a></li>
+	<li><a href="/abc_caracteristica">Caracteristica</a></li>
+	<li><a href="/abc_usuario">Usuario</a></li>
 @stop
