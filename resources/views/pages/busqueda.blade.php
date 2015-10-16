9f9040102259ceeb107660c3d5902754bdab6be5
Carlos Rodriguez <carlosrod.furlan@gmail.com>
2015-10-16 17:43:47 -0600

    Correcciones

--- resources/views/pages/busqueda.blade.php
+++ resources/views/pages/busqueda.blade.php
@@ -18,7 +18,33 @@
 	<tr><th>Establecimiento</th><th>Descripcion</th><th>Calificacion</th></tr>
 	@foreach ($resultados as $resultado)
 		<tr>
-		<td><a href="/establecimiento/{{$resultado->establecimiento}}">{{$resultado->nombre}}</a></td><td>{{$resultado->descripcion}}</td><td>{{$resultado->punteo}}</td>
+		<td>
+			<a href="/establecimiento/{{$resultado->establecimiento}}">{{$resultado->nombre}}</a>
+		</td>
+		<td>
+			{{$resultado->descripcion}}
+		</td>
+		<td>
+			<td>
+			@if ($resultado->punteo > 6)
+				<img src="/images/7balls.png" alt="7 ball rating" style="width:100px;height:15px">
+			@elseif ($resultado->punteo > 5)
+				<img src="/images/6balls.png" alt="7 ball rating" style="width:100px;height:15px">
+			@elseif ($resultado->punteo > 4)
+				<img src="/images/5balls.png" alt="7 ball rating" style="width:100px;height:15px">
+			@elseif ($resultado->punteo > 3)
+				<img src="/images/4balls.png" alt="7 ball rating" style="width:100px;height:15px">
+			@elseif ($resultado->punteo > 2)
+				<img src="/images/3balls.png" alt="7 ball rating" style="width:100px;height:15px">
+			@elseif ($resultado->punteo > 1)
+				<img src="/images/2balls.png" alt="7 ball rating" style="width:100px;height:15px">
+			@elseif ($resultado->punteo > 0)
+				<img src="/images/1balls.png" alt="7 ball rating" style="width:100px;height:15px">
+			@else
+				<img src="/images/RNA.png" alt="7 ball rating" style="width:100px;height:15px">
+			@endif
+		</td>
+		</td>
 		</tr>
 	@endforeach
 	</table>
--- resources/views/pages/normalProfile.blade.php
+++ resources/views/pages/normalProfile.blade.php
@@ -3,3 +3,8 @@
 @section('title')
 	Perfil
 @stop
+
+@section('nav_items')
+	<li class="current"><a href="#">Perfil</a></li>
+	<li><a href="/misReservas">Reservas</a></li>
+@stop
