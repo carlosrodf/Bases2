43ea85e633f822a2cb889d8bb44faadf7b0c191c
pruebas <pruebas@prueba.gt>
2015-10-15 18:26:04 -0600

    abc establecimiento y tipo

---
+++ resources/views/abc/abc_index.blade.php
@@ -0,0 +1,16 @@
+@extends('app')
+
+@section('title')
+	ABC INDEX
+@stop
+
+	@if (isset($error))
+		{{ $error }}
+	@endif
+
+@stop
+
+@section('nav_items')
+	<li><a href="/">Principal</a></li>
+	<li class="current"><a href="#">Registro</a></li>
+@stop
--- resources/views/abc/establecimiento.blade.php
+++ resources/views/abc/establecimiento.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('header')
-	<h2>Creacion de establecimiento</h2>
+	<h2>ABC Establecimiento</h2>
   <hr>
 @stop
 
@@ -14,6 +14,8 @@
 @stop
 
 @section('body')
+<h3>Creacion de Establecimiento</h3>
+<hr>
 	{!! Form::open(['url' => 'a_establecimiento']) !!}
 		{!! Form::label('Nombre:') !!}
 		{!! Form::text('nombre') !!}
@@ -22,10 +24,40 @@
 		{!! Form::label('Descripcion:') !!}
 		{!! Form::text('descripcion') !!}
 		{!! Form::label('Tipo:') !!}
-		{!! Form::text('tipo') !!}
+		{!! Form::select('tipo',$opciones, array_values($opciones)[0]) !!}
 		{!! Form::label('Oficial:') !!}
-		{!! Form::text('oficial') !!}
+		{!! Form::checkbox('oficial', '1') !!}
 		{!! Form::submit('Crear') !!}
+	{!! Form::close() !!}
+
+
+	<h3>Eliminacion de Establecimiento</h3>
+	<hr>
+
+	{!! Form::open(['url' => 'b_establecimiento']) !!}
+		{!! Form::label('Nombre:') !!}
+		{!! Form::select('id',$opciones2, array_values($opciones2)[0]) !!}
+		{!! Form::submit('Borrar') !!}
+	{!! Form::close() !!}
+
+	<h3>Actualizacion de Establecimiento</h3>
+	<hr>
+	{!! Form::open(['url' => 'c_establecimiento']) !!}
+		{!! Form::label('Establecimiento a actualizar:') !!}
+		{!! Form::select('id',$opciones2, array_values($opciones2)[0]) !!}
+		{!! Form::label('Nombre:') !!}
+		{!! Form::text('nombre') !!}
+		{!! Form::label('Posicion:') !!}
+		{!! Form::text('posicion') !!}
+		{!! Form::label('Descripcion:') !!}
+		{!! Form::text('descripcion') !!}
+		{!! Form::label('Punteo:') !!}
+		{!! Form::text('punteo') !!}
+		{!! Form::label('Tipo:') !!}
+		{!! Form::select('tipo',$opciones, array_values($opciones)[0]) !!}
+		{!! Form::label('Oficial:') !!}
+		{!! Form::checkbox('oficial', '1') !!}
+		{!! Form::submit('Actualizar') !!}
 	{!! Form::close() !!}
 
 	@if (isset($error))
